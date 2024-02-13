b7ba53b837f65eb7df3814d37a6d96b78b5c88ed
Takayuki Kurosawa <tkuro11@gmail.com>
2024-02-14 08:10:56 +0900

    fix to calculation of magnitude (round to nearest)

--- octaver.py
+++ octaver.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python
 import sys
+import os
 import math
 
-interval = "C C# D D# E F F# G G# A A# B".split()
+notes = "C _ C# Db D _ D# Eb E _ F _ F# Gb G _ G# Ab A _ A# Bb B _".split()
+# compute basic frequency coefficient from note A (=440Hz).
 # alpha * (2 ** 1/12) ** n  =  440
-#  n = 12 + 12 + 9 (cddeffgga)
-#  alpha = 440 / 2 ** 1/12 ** 33
-alpha = 440 / ((2**(1/12)) ** 33)
+#  where n = 12 + 12 + 9 = 33
+alpha = 110 / (4*((2**(1/12)) ** 9))
 
 arg = sys.argv[1]
-if arg.isdigit():
-    hz = int(arg)
-    # alpha * (2 ** 1/12) ** idx  =  hz
-    # hz/alpha = (2 ** 1/12) ** idx
-    # log(hz/alpha) = idx * log(2**1/12)
-    # log(hz/alpha) / log(2 ** 1/12) = idx
-    idx = int(math.log(hz/alpha) / math.log(2)*12)
+if arg[0].isdigit():
+    hz = float(arg)
+    idx = int(math.log(hz/alpha) / math.log(2) *12 + 0.5)
 
-    print(f"O{int(idx/12)} {interval[idx % 12]}")
+    print(f"O{int(idx/12)} {notes[(idx % 12)*2]}")
 else:
     if arg[0].upper() == "O":
         octave = int(arg[1])
         if len(sys.argv) == 3:
-            arg = sys.argv[2].upper()
+            arg = sys.argv[2].capitalize()
         else:
-            arg = "".join(arg[2:]).upper()
+            arg = "".join(arg[2:]).capitalize()
     else:
-        arg = sys.argv[1].upper()
-        print("> no octave specified, assuming O2")
-        octave = 2
-    idx = interval.index(arg)
-    print(f"f = {alpha*2**((idx + octave*12)/12):.3f} Hz")
+        print("> no octave specified, assuming O4", file=sys.stderr)
+        arg = sys.argv[1].capitalize()
+        octave = 4
 
-
+    idx = notes.index(arg)//2
+    f = alpha*2**((idx + octave*12)/12)
+    if os.isatty(1):
+        print(f"f = {f:.3f} Hz")
+    else:
+        print(f"{f:.4f}")
