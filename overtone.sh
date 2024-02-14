43829b2d8cc0b9ce30bee6bce41595f04308aa0a
Takayuki Kurosawa <tkuro11@gmail.com>
2024-02-14 15:28:01 +0900

    use bc to calculate floating point arithmatic

--- overtone.sh
+++ overtone.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-if [ ! $1 ]; then
+if [ -z "$1" ]; then
     echo "Usage $1 <NOTE NAME> (#count)"
     exit 1
 fi
 
 base=$(./octaver.py $1)
 
-if [ $2 ]; then to=$2 else to=8 fi
+if [ $2 ]; then to=$2
+else
+    to=8
+fi
 
 for i in `seq 1 $to`; do
     echo -n "x$i "
-    ./octaver.py $((i*base))
+    ./octaver.py `echo $i*$base|bc`
 done
