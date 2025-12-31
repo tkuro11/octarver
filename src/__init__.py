4e939f9016ae89b3419965df32904d506c60c2bf
Takayuki Kurosawa <tkuro11@gmail.com>
2025-12-31 21:51:42 +0900

    maintainance update

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,6 @@ Convert between musical note names and frequencies.
 
 from .__main__ import note2freq, freq2note, main
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["freq2note", "note2freq", "main"]
 
