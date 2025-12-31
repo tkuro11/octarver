f87ffd67022fc2e144dcecaf0ec69ee6aac9eb62
Takayuki Kurosawa <tkuro11@gmail.com>
2025-12-31 21:49:19 +0900

    rearrange structure of repository to standard package layout

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Octarver - Octave Range Converter
+
+Convert between musical note names and frequencies.
+"""
+
+from .__main__ import note2freq, freq2note, main
+
+__version__ = "0.1.1"
+__all__ = ["freq2note", "note2freq", "main"]
+
